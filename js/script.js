27e0ae36875ab5f6f7ac0706b8b068c1a3450953
akash-infocom <112191907+akash-infocom@users.noreply.github.com>
2022-09-22 12:08:37 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let notification = document.querySelector('.activity-log .nav-icon')
+let activityContainer = document.querySelector('.activity-container')
+
+notification.addEventListener('click',()=>{
+
+activityContainer.classList.toggle('hide');
+
+})
